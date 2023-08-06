3e15f069d94c1e223553167ba5190e2ffdb25fc2
Yakov29 <108018683+Yakov29@users.noreply.github.com>
2023-08-06 08:49:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 const users = [
-    {
-      id: 1,
-      name: "John",
-      balance: 100,
-      friends: ["Mike", "Anna", "Elena"],
-      skills: ["JavaScript", "HTML", "CSS"],
-    },
-    {
-      id: 2,
-      name: "Mike",
-      balance: 150,
-      friends: ["John", "Anna"],
-      skills: ["JavaScript", "Python", "Java"],
-    },
-  ];
-  
+  {
+    id: 1,
+    name: "John",
+    balance: 100,
+    friends: ["Mike", "Anna", "Elena"],
+    skills: ["JavaScript", "HTML", "CSS"],
+  },
+  {
+    id: 2,
+    name: "Mike",
+    balance: 150,
+    friends: ["John", "Anna"],
+    skills: ["JavaScript", "Python", "Java"],
+  },
+];
 // Завдання 1
-//   function getUsersWithFriend(users, friendName) {
-//     return users.filter(user => user.friends.includes(friendName)).map(user => user.name);
-//   }
-  
-//   const friendName = "Mike";
-//   const usersWithFriend = getUsersWithFriend(users, friendName);
-//   console.log(`Користувачі, у яких є друг ${friendName}:`, usersWithFriend);
+// const totalBalance = users.reduce((acc, {balance}) => {
+//   return balance + acc
+// }, 0)
+// console.log(totalBalance)
   
 
   // Завдання 2
@@ -43,15 +39,24 @@ const users = [
   
 //   const usersSortedByFriendsCount = sortUsersByFriendsCount(users);
 //   console.log("Користувачі, відсортовані за кількістю друзів:", usersSortedByFriendsCount);
-  
-// Завдання 4
-// function getAllUniqueSkills(users) {
-//     const allSkills = users.flatMap(user => user.skills);
-//     const uniqueSkills = [...new Set(allSkills)];
-//     return uniqueSkills.sort();
-//   }
-  
 
-//   const allUniqueSkills = getAllUniqueSkills(users);
-//   console.log("Усі унікальні уміння користувачів:", allUniqueSkills);
+
+// Завдання 4
+// const skills = users.filter(({skills}, index, users) => {
+//   const usersIndex = users.indexOf(...skills) === index;
+//   return usersIndex;
+// })
+// console.log(skills)
+
+// const skills = users.map((user) => 
+//    user.skills
+// )
+// console.log(skills)
+// function test (a) {
   
+// }
+
+
+
+
+
